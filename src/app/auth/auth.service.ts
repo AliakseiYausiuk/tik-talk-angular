56b534f04e63625525f0ef1de82acd01afcd0be9
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-06-28 15:26:56 +0300

    docs: deleted img

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
+import { TokenResponse } from './auth.interface';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  cookieService = inject(CookieService);
+  baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
+
+  token: string | null = null;
+  refreshToken: string | null = null;
+
+  get isAuth() {
+    if (!this.token) {
+      this.token = this.cookieService.get('token');
+      this.refreshToken = this.cookieService.get('refreshToken');
+    }
+    return !!this.token;
+  }
+
+  login(payload: { username: string; password: string }) {
+    const fd = new FormData();
+
+    fd.append('username', payload.username);
+    fd.append('password', payload.password);
+
+    return this.http
+      .post<TokenResponse>(`${this.baseApiUrl}token`, fd)
+      .pipe(tap((value) => this.saveTokens(value)));
+  }
+
+  refreshAuthToken() {
+    return this.http
+      .post<TokenResponse>(`${this.baseApiUrl}refresh`, {
+        refresh_token: this.refreshToken,
+      })
+      .pipe(
+        tap((value) => this.saveTokens(value)),
+        catchError((err) => {
+          this.logout();
+          return throwError(err);
+        })
+      );
+  }
+
+  logout() {
+    this.cookieService.deleteAll();
+    this.token = null;
+    this.refreshToken = null;
+    this.router.navigate(['/login']);
+  }
+
+  saveTokens(res: TokenResponse) {
+    this.token = res.access_token;
+    this.refreshToken = res.refresh_token;
+
+    this.cookieService.set('token', this.token);
+    this.cookieService.set('refreshToken', this.refreshToken);
+  }
+}
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss',
+})
+export class LoginPageComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  form = new FormGroup({
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      //@ts-ignore
+      this.authService.login(this.form.value).subscribe((res) => {
+        this.router.navigate(['']);
+        console.log(res);
+      });
+    }
+  }
+}
