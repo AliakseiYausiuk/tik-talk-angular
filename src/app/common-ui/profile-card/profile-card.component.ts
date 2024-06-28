04578b8bb268c3f4aef9cbf28209d9ef4bd80777
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-06-28 09:32:29 +0300

    feat: add profile-card component

---
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
+
+@Component({
+  selector: 'app-profile-card',
+  standalone: true,
+  templateUrl: './profile-card.component.html',
+  styleUrl: './profile-card.component.scss',
+  imports: [ImgUrlPipe],
+})
+export class ProfileCardComponent {
+  @Input() profile!: Profile;
+}
