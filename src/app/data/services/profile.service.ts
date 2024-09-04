01b83b5e78d3aaae6061c062f436c06a34f42074
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-04 14:16:08 +0300

    feat: add metod service

--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,20 +1,31 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { Profile } from '../interfaces/profile.interface';
+import { Pageble } from '../interfaces/pageble.interface';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
   http = inject(HttpClient);
-
   baseApiUrl = 'https://icherniakov.ru/yt-course/';
 
-  getTestAccounts() {
-    return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`);
+  me = signal<Profile | null>(null);
+
+  getAccount(id: string) {
+    return this.http.get<Profile[]>(`${this.baseApiUrl}account/${id}`);
   }
 
   getMe() {
-    return this.http.get<Profile>(`${this.baseApiUrl}account/me`);
+    return this.http
+      .get<Profile>(`${this.baseApiUrl}account/me`)
+      .pipe(tap((res) => this.me.set(res)));
+  }
+
+  getSubscribersShortList() {
+    return this.http
+      .get<Pageble<Profile>>(`${this.baseApiUrl}account/subscribers/`)
+      .pipe(map((res) => res.items.slice(0, 3)));
   }
 }
