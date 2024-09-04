727723d2ca1804f993e26675c5f6f2edb755e710
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-04 14:12:30 +0300

    fix: rename path in routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,7 +15,7 @@ export const routes: Routes = [
         component: SearchPageComponent,
       },
       {
-        path: 'profile',
+        path: 'profile/:id',
         component: ProfilePageComponent,
       },
     ],
