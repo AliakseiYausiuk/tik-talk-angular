1f7b2504abdc747d3b54d8bf47c7932c10597b7e
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-04 14:13:54 +0300

    feat: add profile-header

---
+++ src/app/common-ui/profile-header/profile-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
+
+@Component({
+  selector: 'app-profile-header',
+  standalone: true,
+  imports: [ImgUrlPipe],
+  templateUrl: './profile-header.component.html',
+  styleUrl: './profile-header.component.scss',
+})
+export class ProfileHeaderComponent {
+  profile = input<Profile>();
+  // @Input() profile!: Profile;
+}
