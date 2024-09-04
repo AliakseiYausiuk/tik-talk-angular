27f4bfc127127381c28257a61f69f18379337d88
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-04 14:14:20 +0300

    feat: add sidebar

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SvgComponent } from '../svg/svg.component';
+import { NgFor } from '@angular/common';
+import { SubscriberCardComponent } from './subscriber-card/subscriber-card.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { ProfileService } from '../../data/services/profile.service';
+import { firstValueFrom } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    SvgComponent,
+    NgFor,
+    SubscriberCardComponent,
+    RouterLink,
+    RouterOutlet,
+    AsyncPipe,
+    ImgUrlPipe
+],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  profileService = inject(ProfileService);
 
+  subscribers$ = this.profileService.getSubscribersShortList();
+
+  me = this.profileService.me;
+
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: 'profile/me',
+    },
+    {
+      label: 'Чаты',
+      icon: 'icon-chat',
+      link: 'chats',
+    },
+    {
+      label: 'Поиск',
+      icon: 'icon-search',
+      link: 'search',
+    },
+  ];
+
+  ngOnInit() {
+    firstValueFrom(this.profileService.getMe());
+  }
 }
