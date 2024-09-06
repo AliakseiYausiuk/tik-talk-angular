b926464806615278078c972521c822802ac9dcfc
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-06 16:53:45 +0300

    feat: add component

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProfileHeaderComponent } from '../../common-ui/profile-header/profile-header.component';
+import { ProfileService } from '../../data/services/profile.service';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [],
+  imports: [ProfileHeaderComponent, AsyncPipe, RouterLink, RouterOutlet],
   templateUrl: './profile-page.component.html',
-  styleUrl: './profile-page.component.scss'
+  styleUrl: './profile-page.component.scss',
 })
 export class ProfilePageComponent {
+  profileService = inject(ProfileService);
+  route = inject(ActivatedRoute);
 
+  me$ = toObservable(this.profileService.me);
+
+  profile$ = this.route.params.pipe(
+    switchMap(({ id }) => {
+      if (id === 'me') return this.me$;
+
+      return this.profileService.getAccount(id);
+    })
+  );
 }
