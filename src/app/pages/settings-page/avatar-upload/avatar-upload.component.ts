f366f0a215d80b19ea801ef25bb0c1a863c8fa72
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-08 13:10:39 +0300

    feat: add avatar component

---
+++ src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -0,0 +1,34 @@
+import { Component, signal } from '@angular/core';
+import { SvgComponent } from '../../../common-ui/svg/svg.component';
+import { DndDirective } from '../../../common-ui/directives/dnd.directive';
+
+@Component({
+  selector: 'app-avatar-upload',
+  standalone: true,
+  imports: [SvgComponent, DndDirective],
+  templateUrl: './avatar-upload.component.html',
+  styleUrl: './avatar-upload.component.scss',
+})
+export class AvatarUploadComponent {
+  preview = signal<string>('assets/svg/avatar-placeholder.svg');
+
+  avatar: File | null = null;
+
+  fileBrowserHandler(event: Event) {
+    const file = (event.target as HTMLInputElement)?.files?.[0];
+    this.processFile(file);
+  }
+
+  processFile(file: File | null | undefined) {
+    if (!file || file.type.match('image')) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      this.preview.set(event.target?.result?.toString() ?? '');
+    };
+
+    reader.readAsDataURL(file);
+    this.avatar = file;
+  }
+}
