9a2e78703fd3a56d2b069b221bc32bff7da08964
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-08 10:40:01 +0300

    feat: add dependence

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -6,11 +6,22 @@ import { switchMap } from 'rxjs';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { SvgComponent } from '../../common-ui/svg/svg.component';
+import { ImgUrlPipe } from "../../helpers/pipes/img-url.pipe";
+import { PostFeedComponent } from "./post-feed/post-feed.component";
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [ProfileHeaderComponent, AsyncPipe, RouterLink, RouterOutlet],
+  imports: [
+    ProfileHeaderComponent,
+    AsyncPipe,
+    RouterLink,
+    RouterOutlet,
+    SvgComponent,
+    ImgUrlPipe,
+    PostFeedComponent
+],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
 })
@@ -19,6 +30,7 @@ export class ProfilePageComponent {
   route = inject(ActivatedRoute);
 
   me$ = toObservable(this.profileService.me);
+  subscribers$ = this.profileService.getSubscribersShortList(5);
 
   profile$ = this.route.params.pipe(
     switchMap(({ id }) => {
