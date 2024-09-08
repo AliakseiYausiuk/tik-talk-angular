07c50ed5358528a8f6c39942a56accb669b2e9cb
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-08 13:07:01 +0300

    feat: add directive dnd

---
+++ src/app/common-ui/directives/dnd.directive.ts
@@ -0,0 +1,41 @@
+import {
+  Directive,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Output,
+} from '@angular/core';
+
+@Directive({
+  selector: '[dnd]',
+  standalone: true,
+})
+export class DndDirective {
+  @Output() fileDropped = new EventEmitter<File>();
+
+  @HostBinding('class.fileover')
+  fileover = false;
+
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileover = true;
+  }
+
+  @HostListener('dragLeave', ['$event'])
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileover = false;
+  }
+
+  @HostListener('drop', ['$event'])
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.fileover = false;
+    this, this.fileDropped.emit(event.dataTransfer?.files[0]);
+  }
+}
