f17561113e57704caf92e70a1438ec97f768a05a
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-10 15:05:33 +0300

    fix: change get profiles

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -2,21 +2,21 @@ import { Component, inject } from '@angular/core';
 import { ProfileCardComponent } from "../../common-ui/profile-card/profile-card.component";
 import { ProfileService } from '../../data/services/profile.service';
 import { Profile } from '../../data/interfaces/profile.interface';
+import {ProfileFiltersComponent} from "./profile-filters/profile-filters.component";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
     selector: 'app-search-page',
     standalone: true,
     templateUrl: './search-page.component.html',
     styleUrl: './search-page.component.scss',
-    imports: [ProfileCardComponent]
+  imports: [ProfileCardComponent, ProfileFiltersComponent, AsyncPipe]
 })
 export class SearchPageComponent {
   profileService = inject(ProfileService);
-  profiles: Profile[] = [];
+  profiles = this.profileService.filteredProfiles
 
   constructor() {
-    this.profileService.getTestAccounts().subscribe((val) => {
-      this.profiles = val;
-    });
+
   }
 }
