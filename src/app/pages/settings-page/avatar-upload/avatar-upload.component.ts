2a20f64cc1c8e1f7db013cb5ffd8fb5006d491cc
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-10 13:26:33 +0300

    fix: add import FormModule and fix functions

--- src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
+++ src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { SvgComponent } from '../../../common-ui/svg/svg.component';
 import { DndDirective } from '../../../common-ui/directives/dnd.directive';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-avatar-upload',
   standalone: true,
-  imports: [SvgComponent, DndDirective],
+  imports: [SvgComponent, DndDirective, FormsModule],
   templateUrl: './avatar-upload.component.html',
   styleUrl: './avatar-upload.component.scss',
 })
@@ -16,11 +17,16 @@ export class AvatarUploadComponent {
 
   fileBrowserHandler(event: Event) {
     const file = (event.target as HTMLInputElement)?.files?.[0];
+
+    this.processFile(file);
+  }
+
+  onFileDropped(file: File) {
     this.processFile(file);
   }
 
   processFile(file: File | null | undefined) {
-    if (!file || file.type.match('image')) return;
+    if (!file || !file.type.match('image')) return;
 
     const reader = new FileReader();
 
