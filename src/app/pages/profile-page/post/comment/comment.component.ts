4311fa98ecaacde522199854d6e9b25d48c5d184
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-12 14:33:38 +0300

    fead: add component Comment

---
+++ src/app/pages/profile-page/post/comment/comment.component.ts
@@ -0,0 +1,18 @@
+import {Component, input} from '@angular/core';
+import {PostComment} from "../../../../data/interfaces/post.interface";
+import {AvatarCircleComponent} from "../../../../common-ui/avatar-circle/avatar-circle.component";
+import {DatePipe} from "@angular/common";
+
+@Component({
+  selector: 'app-comment',
+  standalone: true,
+  imports: [
+    AvatarCircleComponent,
+    DatePipe
+  ],
+  templateUrl: './comment.component.html',
+  styleUrl: './comment.component.scss'
+})
+export class CommentComponent {
+  comment = input<PostComment>()
+}
