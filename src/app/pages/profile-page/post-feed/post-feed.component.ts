ef717f15e1e976c08bd3452302586f8a50c13f6e
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-12 14:36:46 +0300

    feat: add logic component

--- src/app/pages/profile-page/post-feed/post-feed.component.ts
+++ src/app/pages/profile-page/post-feed/post-feed.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { PostInputComponent } from "../post-input/post-input.component";
-import { PostComponent } from "../post/post.component";
+import {AfterViewInit, Component, ElementRef, HostListener, inject, Renderer2} from '@angular/core';
+import {PostInputComponent} from "../post-input/post-input.component";
+import {PostComponent} from "../post/post.component";
+import {PostService} from "../../../data/services/post.service";
+import {firstValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-post-feed',
@@ -9,6 +11,29 @@ import { PostComponent } from "../post/post.component";
   templateUrl: './post-feed.component.html',
   styleUrl: './post-feed.component.scss'
 })
-export class PostFeedComponent {
+export class PostFeedComponent implements AfterViewInit {
+  postService = inject(PostService);
+  hostElement = inject(ElementRef);
+  r2 = inject(Renderer2)
 
+  feed = this.postService.posts;
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.resizeFeed()
+  }
+
+  constructor() {
+    firstValueFrom(this.postService.fetchPosts())
+  }
+
+  ngAfterViewInit() {
+    this.resizeFeed()
+  }
+
+  resizeFeed() {
+    const {top} = this.hostElement.nativeElement.getBoundingClientRect();
+    const height = window.innerHeight - top - 24 - 24
+    this.r2.setStyle(this.hostElement.nativeElement, 'height', `${height}px`);
+  }
 }
