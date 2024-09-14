e51534cc6b7589fff82230798c1d828042cda4a5
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-14 14:40:49 +0300

    feat: add chat-workspace-header component

---
+++ src/app/pages/chat-page/chat-workspace/chat-workspace-header/chat-workspace-header.component.ts
@@ -0,0 +1,17 @@
+import {Component, input} from '@angular/core';
+import {Profile} from "../../../../data/interfaces/profile.interface";
+import {AvatarCircleComponent} from "../../../../common-ui/avatar-circle/avatar-circle.component";
+
+@Component({
+  selector: 'app-chat-workspace-header',
+  standalone: true,
+  imports: [
+    AvatarCircleComponent
+  ],
+  templateUrl: './chat-workspace-header.component.html',
+  styleUrl: './chat-workspace-header.component.scss'
+})
+export class ChatWorkspaceHeaderComponent {
+  profile = input.required<Profile>()
+
+}
