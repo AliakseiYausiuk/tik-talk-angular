f3379f6f0e55c116d97d5f4a3bf3fa930b51165d
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-14 14:41:43 +0300

    feat: add chats service

---
+++ src/app/data/services/chats.service.ts
@@ -0,0 +1,59 @@
+import {inject, Injectable, signal} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Chat, LastMessageRes, Message} from "../interfaces/chats.interface";
+import {ProfileService} from "./profile.service";
+import {map} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ChatService {
+  http = inject(HttpClient)
+  me = inject(ProfileService).me;
+
+  activeChatMessages = signal<Message[]>([])
+
+  baseApiUrl = 'https://icherniakov.ru/yt-course/';
+  chatsUrl = `${this.baseApiUrl}chat/`;
+  messageUrl = `${this.baseApiUrl}message/`;
+
+  createChat(userId: number) {
+    return this.http.post<Chat>(`${this.chatsUrl}${userId}`, {});
+  }
+
+  getMeChats() {
+    return this.http.get<LastMessageRes[]>(`${this.chatsUrl}get_my_chats/`);
+  }
+
+  getChatById(chatId: number) {
+    return this.http.get<Chat>(`${this.chatsUrl}${chatId}`)
+      .pipe(
+        map(chat => {
+          const patchedMessages = chat.messages.map(message => {
+            return {
+              ...message,
+              user: chat.userFirst.id === message.userFromId ? chat.userFirst : chat.userSecond,
+              isMine: message.userFromId === this.me()!.id,
+            }
+          })
+
+          this.activeChatMessages.set(patchedMessages)
+
+          return {
+            ...chat,
+            companion: chat.userFirst.id === this.me()!.id ? chat.userSecond : chat.userFirst,
+            messages: patchedMessages
+          }
+        })
+      )
+  }
+
+  sendMessage(chatId: number, message: string) {
+    return this.http.post<Message>(`${this.messageUrl}send/${chatId}`, {}, {
+      params: {
+        message
+      }
+    });
+  }
+}
