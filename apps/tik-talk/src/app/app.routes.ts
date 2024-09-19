f82f7209610a5e83cf071f29b111a44dfa959c00
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-19 20:01:56 +0300

    feat: add provideStore and provideEffects

--- apps/tik-talk/src/app/app.routes.ts
+++ apps/tik-talk/src/app/app.routes.ts
@@ -2,7 +2,16 @@ import { Routes } from '@angular/router';
 import {canActivateAuth, LoginPageComponent} from '@tt/auth';
 import {chatsRoutes} from '@tt/chats';
 import {LayoutComponent} from '@tt/layout';
-import {ProfilePageComponent, SearchPageComponent, SettingsPageComponent} from '@tt/profile';
+import {
+  ProfileEffects,
+  profileFeature,
+  ProfilePageComponent,
+  SearchPageComponent,
+  SettingsPageComponent
+} from '@tt/profile';
+import {provideState} from "@ngrx/store";
+import {provideEffects} from "@ngrx/effects";
+
 
 export const routes: Routes = [
   {
@@ -12,7 +21,14 @@ export const routes: Routes = [
       { path: '', redirectTo: 'profile/me', pathMatch: 'full' },
       { path: 'profile/:id', component: ProfilePageComponent },
       { path: 'settings', component: SettingsPageComponent },
-      { path: 'search', component: SearchPageComponent },
+      {
+        path: 'search',
+        component: SearchPageComponent,
+        providers: [
+          provideState(profileFeature),
+          provideEffects(ProfileEffects)
+        ]
+      },
       {
         path: 'chats',
         loadChildren: () => chatsRoutes,
