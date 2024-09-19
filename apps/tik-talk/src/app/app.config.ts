15891a4a1efb5704f6de46c01d6dcbc7412240e8
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-19 20:01:24 +0300

    feat: add provideStore

--- apps/tik-talk/src/app/app.config.ts
+++ apps/tik-talk/src/app/app.config.ts
@@ -1,14 +1,17 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { authTokenInterceptor } from '@tt/auth';
+import {provideStore} from "@ngrx/store";
+import {provideEffects} from "@ngrx/effects";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authTokenInterceptor])),
+    provideStore(),
+    provideEffects()
   ],
 };
