1a75c98993fe946a424589a48214927779a50d12
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-09-26 13:12:38 +0300

    feat: add components

---
+++ libs/experimental/src/lib/experimental/experimental.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'tt-experimental',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './experimental.component.html',
+  styleUrl: './experimental.component.scss',
+})
+export class ExperimentalComponent {}
